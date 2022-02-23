c5d1c78b47265a2bdae78c4662bb49a65088fdd9
ImprovingTyler <TylerJamesLundin@gmail.com>
2022-02-22 21:46:48 -0800

    Added Nav, created file for content section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import styled from 'styled-components'
+import Nav from './components/Nav/Nav';
 
 const S = {}
 
 S.App = styled.div`
-
+  width: 100vw;
+  min-height: 100vh;
 `;
 
 function App() {
   return (
     <S.App>
-      
+      <Nav/>
     </S.App>
   );
 }
---
+++ src/components/Content/Content.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const S = {}
+S.Content = styled.div`
+    width: 100vw;
+    min-height: calc(100vh - 3rem);
+`;
+
+const Content = () => {
+
+
+    return (
+        <S.Content>
+
+        </S.Content>
+    )
+}
+
+export default Content
